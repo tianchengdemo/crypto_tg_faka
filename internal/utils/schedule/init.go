bb5c7a9fdcfa7c4012b6c8b76b02707011a5d377
null <wabirefolus6536@gmail.com>
2023-12-03 14:46:43 +0800

    init

--- internal/utils/schedule/init.go
+++ internal/utils/schedule/init.go
@@ -24,7 +24,7 @@ func CheckTransactionSchedule() {
 		//TRON
 		startCheckTransaction("TRON")
 		//POLYGON
-		startCheckTransaction("POLYGON")
+		//startCheckTransaction("POLYGON")
 		time.Sleep(checkTransactionInterval)
 	}
 }
